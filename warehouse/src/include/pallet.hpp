19d62e314d6ab03d178254efcba794b7934b99ac
Jesse <jr.egmond@ziggo.nl>
2023-05-24 22:52:21 +0200

    Added pallet hpp

---
+++ warehouse/src/include/pallet.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+
+class Pallet {
+    private:
+        int itemCount; // Only mutable through taking and putting
+        std::string itemName; // Only mutable if empty
+        int itemCapacity; // Only mutable if empty
+
+    public:
+        // Pallet constructors
+        Pallet(std::string itemName, int itemCapacity, int itemCount);
+        Pallet();
+
+        // Getters class variables
+        std::string getitemName();
+        int getitemCount();
+        
+        int getRemainingSpace();
+        bool reallocateEmptyPallet(std::string itemName, int itemCapacity);
+        bool takeOne();
+        bool putOne();
+};
